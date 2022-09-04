7ebda528d22982a413faea05a956d91986047974
André Torres <andretorresjava@gmail.com>
2022-09-03 21:09:05 -0500

    Building the About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,27 @@
-import { AboutContainer, AboutDiv } from './styles';
+import { FaHtml5, FaCss3Alt, FaSass, FaReact } from 'react-icons/fa';
+import { SiJavascript, SiStyledcomponents } from  'react-icons/si';
+
+import { AboutContainer, AboutDiv, ListItem, Skills } from './styles';
 
 const About = () => {
   return (
     <AboutContainer>
       <AboutDiv>
-        <h1>About me</h1>
-        <p>Hello! My name is Héctor André, I am a university student who likes to improve myself professionally, I enjoy creating web applications and I love learning the English language. My interest in web development started when I was building a responsive design application on top of a portfolio made in HTML5, CSS, JavaScript and the SwiperJS library. Then I got really excited learning React, practicing it with Frontend Mentor challenges and reviewing other programmers' code on GitHub.</p>
+        <h1>Do you know who I am?</h1>
+        <p>Hello! My name is Héctor André, I am a university student who likes to improve myself professionally, I enjoy creating web applications and I love learning the English language.</p>
+        <p>My interest in web development started when I was building a responsive design application on top of a portfolio made in HTML5, CSS, JavaScript and the SwiperJS library. Then I got really excited learning React, practicing it with Frontend Mentor challenges and reviewing other programmers' code on GitHub.</p>
       </AboutDiv>
-      <div>
-
-      </div>
+      <Skills>
+        <h2>Skills</h2>
+        <ul>
+          <ListItem><FaHtml5 color="#dd5237" size="6rem" />html5</ListItem>
+          <ListItem><FaCss3Alt color="#2396f3" size="3rem" />css3</ListItem>
+          <ListItem><SiJavascript color="#f7e038" size="3rem" />javascript</ListItem>
+          <ListItem><FaSass color="#ca6598" size="3rem" />sass</ListItem>
+          <ListItem><FaReact color="#61dafb" size="3rem" />react</ListItem>
+          <ListItem><SiStyledcomponents color="#ffd45d" size="3rem" />styled components 💅🏾</ListItem>
+        </ul>
+      </Skills>
     </AboutContainer>
   )
 }
--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -9,7 +9,47 @@ export const AboutDiv = styled.div`
     width: 90%;
     margin: 0 auto;
     max-width: 1400px;
-    display: flex;
+    padding-top: 11rem;
+    /* display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: center; */
+    /* padding-top: 10rem; */
+    h1{
+        text-align: center;
+        text-transform: uppercase;
+        font-size: ${({theme}) => theme.fonts.sizes.test3};
+        color: ${({theme}) => theme.colors.purple};
+        margin-bottom: 5rem;
+    }
+    p{
+        margin: 0 0 2rem;
+        color: ${({theme}) => theme.colors.gray};
+        font-size: 1.8rem;
+        line-height: 1.3;
+        text-align: justify;
+    }
+`;
+
+export const Skills = styled.div`
+    width: 90%;
+    margin: 0 auto;
+    max-width: 1400px;
+    h2{
+        text-align: center;
+        text-transform: uppercase;
+        font-size: ${({theme}) => theme.fonts.sizes.test3};
+        color: ${({theme}) => theme.colors.purple};
+        margin-bottom: 5rem;
+    }
+    ul{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 2rem;
+    }
+`;
+
+export const ListItem = styled.li`
+    color: ${({theme}) => theme.colors.gray};
+    font-size: 1.8rem;
+    /* Cambiar el text-transform de las tecnologìas */
 `;
