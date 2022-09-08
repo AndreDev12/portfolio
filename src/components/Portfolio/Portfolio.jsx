3802b51df0590b2145755385e9de0f83d705cd1d
André Torres <andretorresjava@gmail.com>
2022-09-07 21:45:02 -0500

    Building the Portfolio component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,13 @@
 import { BsGithub } from 'react-icons/bs';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 
-import { ContentText, PortfolioContainer, PortfolioDiv, Projects } from "./styles";
+import { Card, ContentText, PortfolioContainer, PortfolioDiv, ProjectInfo, ProjectLinks, Projects, ProjectText } from './styles';
+import imgStartupPortfolio from '../../assets/img/startup portfolio.png';
+import imgMarvelCharacters from '../../assets/img/marvel characters.png';
+import imgIntroSectionWithDropdownNavigation from '../../assets/img/intro section with dropdown navigation.png';
+import imgIntroComponentWithSignupForm from '../../assets/img/intro component with sign up form.png';
+import imgIpAddressTracker from '../../assets/img/ip address tracker.png';
+import imgRestCountriesApiWithColorThemeSwitcher from '../../assets/img/rest countries api with color theme switcher.png';
 
 const Portfolio = () => {
   return (
@@ -12,36 +18,44 @@ const Portfolio = () => {
           <p>Check out some projects I've built recently</p>
         </ContentText>
         <Projects>
-          <div>
-            <div>
-              <div>
+          <Card>
+            <img 
+              src={imgStartupPortfolio}
+              alt="Startup portfolio image"
+            />
+            <ProjectInfo>
+              <ProjectText>
                 <h3>Startup portfolio</h3>
                 <p>HTML5, CSS3, JavaScript</p>
-              </div>
-              <div>
+              </ProjectText>
+              <ProjectLinks>
                 <a 
                   href="https://github.com/AndreDev12/startup-portfolio"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <BsGithub color="#f0f6fc" size="3rem" />
+                  <BsGithub color="#f0f6fc" size="2rem" />
                 </a>
                 <a
                   href="https://startup-portfolio-bice.vercel.app/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <FaExternalLinkAlt color="#f0f6fc" size="3rem" />
+                  <FaExternalLinkAlt color="#f0f6fc" size="2rem" />
                 </a>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div>
-              <div>
+              </ProjectLinks>
+            </ProjectInfo>
+          </Card>
+          <Card>
+            <img 
+              src={imgMarvelCharacters}
+              alt='Image of marvel characters'
+            />
+            <ProjectInfo>
+              <ProjectText>
                 <h3>Marvel characters</h3>
                 <p>CSS3, JavaScript</p>
-              </div>
+              </ProjectText>
               <div>
                 <a 
                   href="https://github.com/AndreDev12/marvel-characters"
@@ -58,14 +72,18 @@ const Portfolio = () => {
                   <FaExternalLinkAlt color="#f0f6fc" size="3rem" />
                 </a>
               </div>
-            </div>
-          </div>
-          <div>
-            <div>
-              <div>
+            </ProjectInfo>
+          </Card>
+          <Card>
+            <img 
+              src={imgIntroSectionWithDropdownNavigation}
+              alt='Image of intro section with dropdown navigation'
+            />
+            <ProjectInfo>
+              <ProjectText>
                 <h3>Intro section with dropdown navigation</h3>
                 <p>JavaScript, Sass</p>
-              </div>
+              </ProjectText>
               <div>
                 <a 
                   href="https://github.com/AndreDev12/intro-section-with-dropdown-navigation"
@@ -82,9 +100,13 @@ const Portfolio = () => {
                   <FaExternalLinkAlt color="#f0f6fc" size="3rem" />
                 </a>
               </div>
-            </div>
-          </div>
-          <div>
+            </ProjectInfo>
+          </Card>
+          <Card>
+            <img 
+              src={imgIntroComponentWithSignupForm}
+              alt='Image of intro component with signup form'
+            />
             <div>
               <div>
                 <h3>Intro component with signup form</h3>
@@ -107,8 +129,12 @@ const Portfolio = () => {
                 </a>
               </div>
             </div>
-          </div>
-          <div>
+          </Card>
+          <Card>
+            <img 
+              src={imgIpAddressTracker}
+              alt='Image of ip address tracker'
+            />
             <div>
               <div>
                 <h3>Ip address tracker</h3>
@@ -131,8 +157,12 @@ const Portfolio = () => {
                 </a>
               </div>
             </div>
-          </div>
-          <div>
+          </Card>
+          <Card>
+            <img 
+              src={imgRestCountriesApiWithColorThemeSwitcher}
+              alt='Image of rest countries api with color theme switcher'
+            />
             <div>
               <div>
                 <h3>Rest countries api with color theme switcher</h3>
@@ -155,7 +185,7 @@ const Portfolio = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </Card>
         </Projects>
       </PortfolioDiv>
     </PortfolioContainer>
--- src/components/Portfolio/styles.js
+++ src/components/Portfolio/styles.js
@@ -5,13 +5,14 @@ export const PortfolioContainer = styled.div`
 `;
 
 export const PortfolioDiv = styled.div`
-    height: 100vh;
     width: 90%;
     margin: 0 auto;
     max-width: 1400px;
     padding-top: 11rem; 
-    /* height: 80vh; */
-    /* display: flex; */
+    display: flex;
+    flex-direction: column;
+    /* height: 60vh; */
+    /* height: 100vh; */
 `;
 
 export const ContentText = styled.div`
@@ -28,5 +29,74 @@ export const ContentText = styled.div`
         font-size: 1.8rem;
         line-height: 1.3;
         text-align: center;
+    }
+`;
+
+export const Projects = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-auto-rows: minmax(25rem, auto);
+    row-gap: 1rem;
+`;
+
+export const Card = styled.div`
+    /* padding: 1rem; */
+    /* background: var(--card-background); */
+    color: ${({theme}) => theme.colors.white};
+    /* border-radius: 0.5rem; */
+    transition: 0.4s ease-in-out;
+    border-radius: 0.5rem;
+    cursor: pointer;
+    position: relative;
+    &:hover{
+        /* box-shadow: inset 0 100px 1000px 10px rgba(0, 0, 0, 0.8); */
+        z-index: 50;
+        box-shadow: inset 0 10rem 100rem 1rem ${({theme}) => theme.colors.purple};
+    }
+    &:hover div{
+        opacity: 1;
+    }
+    img{
+        border-radius: 0.5rem;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+export const ProjectInfo = styled.div`
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    right: 1rem;
+    display: flex;
+    justify-content: space-between;
+    opacity: 0;
+    transition: 0.5s ease-in-out;
+`;
+
+export const ProjectText = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    position: relative;
+    top: 16rem;
+    left: 1rem;
+    h3{
+        font-size: 1.4rem;
+        /* color: ${({theme}) => theme.colors.purple}; */
+    }
+    p{
+        font-size: 1.3rem;
+        margin: 0.5rem 0 0;
+        /* color: ${({theme}) => theme.colors.purple}; */
+    }
+`;
+
+export const ProjectLinks = styled.div`
+    a{
+        color: #ffffff;
+        transition: 0.25s ease-in-out;
+        /* font-size: 1.1rem; */
+        margin-right: 0.3rem;
     }
 `;
